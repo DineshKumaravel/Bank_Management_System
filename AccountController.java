7363426db5fbf3c5795851cdd49f8c021ba94b20
DINESH <142299521+DineshKumaravel@users.noreply.github.com>
2025-02-28 15:15:03 +0530

    Update AccountController.java

--- AccountController.java
+++ AccountController.java
@@ -19,18 +19,10 @@ public class AccountController {
         return accountService.getAccountDetails(accNo);
     }
 
-//    @PostMapping("/add-account")
-//    public String addAccount(@RequestBody List<UserAccount> useraccount){
-//         return accountService.addAccount(useraccount);
-//    }
-
-    @PutMapping("/account-number/{accNo}/credit/{money}")
-    public String creditMoney(@PathVariable int accNo, @PathVariable int money){
-        return accountService.creditMoney(accNo,money);
+    @PostMapping("/add-account")
+    public String addAccount(@RequestBody List<UserAccount> useraccount){
+         return accountService.addAccount(useraccount);
     }
 
-    @PutMapping("/account-number/{accNo}/debit/{money}")
-    public String debitMoney(@PathVariable int accNo, @PathVariable int money){
-        return accountService.debitMoney(accNo,money);
-    }
+
 }
